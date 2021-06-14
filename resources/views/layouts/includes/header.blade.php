00a0949af5f89aacc6e41dee5eda6bdc58d115da
tulon <tuloncse@gmail.com>
2021-06-15 01:48:14 +0600

    add bnnmer and info

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,3 +1,25 @@
+<div class="container" style="margin-top: 5px">
+    <div class="card" style="background-color:#f7fafd">
+
+        <div class="row">
+            <div class="col-md-6"><img src="{{asset('assets/img/bannerbg.jpeg')}}" style="width:70%;"/></div>
+            <div class="col-md-6">
+                <div class="loginInfo text-right">
+                    Online Accounting Software<br>
+                    @php
+                    //echo $user = Auth::();
+                    // $data = $request->session()->all();
+                    // print_r($data);
+
+
+                    @endphp<br>
+                    Address<br>
+                    Login Date : 12-Dec-2021</div>
+            </div>
+
+        </div>
+    </div>
+</div>
 <div class="navbar-area">
     <div class="luvion-responsive-nav">
         <div class="container">
@@ -5,8 +27,8 @@
                 <div class="logo">
                     <a href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
-                        <img src="{{asset('assets/img/logo.png')}}" alt="logo">
-                        <img src="{{asset('assets/img/black-logo.png')}}" alt="logo">
+                        {{-- <img src="{{asset('assets/img/logo.png')}}" alt="logo"> --}}
+                        {{-- <img src="{{asset('assets/img/black-logo.png')}}" alt="logo"> --}}
                     </a>
                 </div>
             </div>
@@ -17,8 +39,8 @@
         <div class="container">
             <nav class="navbar navbar-expand-md navbar-light">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('assets/img/logo.png')}}" alt=logo>
-                    <img src="{{asset('assets/img/black-logo.png')}}" alt="logo">
+                    {{-- <img src="{{asset('assets/img/logo.png')}}" alt=logo> --}}
+                    {{-- <img src="{{asset('assets/img/black-logo.png')}}" alt="logo"> --}}
                 </a>
                 <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                     @include('layouts.includes.menu')
